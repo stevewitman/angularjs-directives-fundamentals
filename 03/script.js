35bcc2fa82eef1001f44aa1e247f7c2e752b73fd
Steve Witman <stevewitman@gmail.com>
2015-11-03 23:40:38 -0700

    add nested directive

---
+++ 03/script.js
@@ -0,0 +1,58 @@
+var app = angular.module('app', []);
+
+app.controller('mainCtrl', function($scope) {
+  $scope.user1 = {
+    name: 'Sheldon Cooper',
+    address: { 
+      street: 'PO Box 4453',
+      city: 'Lakewood',
+      state: 'CO'
+    },
+    friends: ['Leonard', 'Raj', 'Howard']
+  }
+  $scope.user2 = {
+    name: 'Leonard Hofstadter',
+    address: { 
+      street: 'PO Box 15322',
+      city: 'Littleton',
+      state: 'CO'
+    },
+    friends: ['Penny', 'Sheldon', 'Raj', 'Howard']
+  }
+})
+
+app.directive('userInfoCard', function() {
+  return {
+    templateUrl: "userInfoCard.html",
+    restrict: "E",
+    scope: {
+      user: '='
+    },
+    controller: function($scope) {
+      $scope.collapsed = false;
+      $scope.won = function(user) {
+        user.status = "waiting";
+      }
+      $scope.collapse = function() {
+        $scope.collapsed = !$scope.collapsed;
+      }
+    }
+  }
+});
+
+app.directive('address',function() {
+  return {
+    restrict: 'E',
+    scope: true,
+    templateUrl: 'address.html',
+    controller: function($scope) {
+      $scope.collapsed = false;
+      $scope.collapseAddress = function() {
+        $scope.collapsed = true;
+      }
+      $scope.expandAddress = function() {
+        $scope.collapsed = false;
+      }
+    }
+  }
+})
