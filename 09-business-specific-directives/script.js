d64ee40d3cef9cfca7a6aec6f761d59093c81ecc
Steve Witman <stevewitman@gmail.com>
2015-11-21 21:27:41 -0700

    business specific directives

---
+++ 09-business-specific-directives/script.js
@@ -0,0 +1,29 @@
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function($scope) {
+  $scope.user1 = {
+    name: 'Luke',
+    selected: false
+  }
+});
+
+angular.module('app').directive('userTile', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      user: '='
+    },
+    templateUrl: 'userTile.html'
+  }
+});
+
+angular.module('app').directive('userClickSelect', function() {
+  return {
+    link: function(scope, el, attrs) {
+      el.on('click', function() {
+        scope.user.selected = !scope.user.selected;
+        scope.$apply();
+      })
+    }
+  }
+})
